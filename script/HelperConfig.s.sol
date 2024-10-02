3edfd892aa1ab8e246655184847037f8740df157
Ghadi Mhawej <ghadimhawej@outlook.com>
2024-10-02 12:31:43 +0300

    feat: helper config refactoring

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -27,9 +27,6 @@ contract HelperConfig is Script {
     uint256 constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 constant LOCAL_CHAIN_ID = 31337;
 
-    uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
-    uint256 constant ZKSYNC_MAINNET_CHAIN_ID = 324;
-
     address constant BURNER_WALLET = 0x23d3957BE879aBa6ca925Ee4F072d1A8C4E8c890;
     address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
@@ -42,8 +39,6 @@ contract HelperConfig is Script {
     constructor() {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getEthSepoliaConfig();
         networkConfigs[ETH_MAINNET_CHAIN_ID] = getEthMainnetConfig();
-        // networkConfigs[ZKSYNC_SEPOLIA_CHAIN_ID] = getZkSyncSepoliaConfig();
-        // networkConfigs[ZKSYNC_MAINNET_CHAIN_ID] = getZkSyncConfig();
     }
 
     function getConfig() public returns (NetworkConfig memory) {
@@ -80,23 +75,7 @@ contract HelperConfig is Script {
             account: BURNER_WALLET
         });
     }
-
-    // function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
-    //     return NetworkConfig({
-    //         entryPoint: address(0), // There is no entrypoint in zkSync!
-    //         usdc: 0x5A7d6b2F92C77FAD6CCaBd7EE0624E64907Eaf3E, // not the real USDC on zksync sepolia
-    //         account: BURNER_WALLET
-    //     });
-    // }
-
-    // function getZkSyncConfig() public pure returns (NetworkConfig memory) {
-    //     return NetworkConfig({
-    //         entryPoint: address(0), // supports native AA, so no entry point needed
-    //         usdc: 0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4,
-    //         account: BURNER_WALLET
-    //     });
-    // }
-
+    
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         if (localNetworkConfig.account != address(0)) {
             return localNetworkConfig;
